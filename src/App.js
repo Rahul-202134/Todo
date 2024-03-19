9ee6a057f26195cf3ed7d4779a1fbf8ad24dc97e
Rahul Kumar <rahulkumar202134@gmail.com>
2024-03-19 14:45:22 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,113 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
-import Todo from './Todo';
-function App() {
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const updateInput = (value) => {
+    setUserInput(value);
+  };
+  const addItem = () => {
+    if (userInput !== "") {
+      const newItem = {
+        id: Math.random(),
+        value: userInput,
+      };
+      setList([...list, newItem]);
+      setUserInput("");
+    }
+  };
+
+  const deleteItem = (id) => {
+    const updatedList = list.filter((item) => item.id !== id);
+    setList(updatedList);
+  };
+
+  const editItem = (index) => {
+    const editedTodo = prompt("Edit the todo:");
+    if (editedTodo !== null && editedTodo.trim() !== "") {
+      const updatedTodos = [...list];
+      updatedTodos[index].value = editedTodo;
+      setList(updatedTodos);
+    }
+  };
+
   return (
-    <>
-    <Todo/>
-    </>
+    <div className="todo">
+ <Container>
+      <Row className="todo-title">
+        TODO LIST
+      </Row>
+
+      <hr />
+      <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="add item . . . "
+              size="lg"
+              value={userInput}
+              onChange={(e) => updateInput(e.target.value)}
+              aria-label="add something"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup>
+              <Button
+                variant="dark"
+                className="mt-2"
+                onClick={addItem}
+              >
+                ADD
+              </Button>
+            </InputGroup>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+          <ListGroup>
+            {list.map((item, index) => (
+              <div key={index}>
+                <ListGroup.Item
+                  variant="dark"
+                  action
+                  className="todo-list-item"
+                >
+                  {item.value}
+                  <span>
+                    <Button
+                      style={{ marginRight: "10px" }}
+                      variant="light"
+                      onClick={() => deleteItem(item.id)}
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      variant="light"
+                      onClick={() => editItem(index)}
+                    >
+                      Edit
+                    </Button>
+                  </span>
+                </ListGroup.Item>
+              </div>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+    </div>
+   
   );
-}
+};
 
 export default App;
